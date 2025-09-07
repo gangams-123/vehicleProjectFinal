d501d98c542f34608f31e2fe6523208f3aae0061
gangams-code <ganga.ms1@gmail.com>
2025-09-07 22:41:15 +0530

    Initial commit with backend and frontend

--- .history/backend/src/routes/offcial.route_20250907191002.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { createOfficialWithAddressAndFile} from "../controllers/official.controller.js"
-const officialRouter = Router();
-
-officialRouter.post("/", createOfficialWithAddressAndFile);
-export default officialRouter;
